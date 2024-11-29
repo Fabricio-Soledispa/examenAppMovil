58e5ebec567d64d9dbabf2d82ba5a83212b9eada
Fabricio Soledispa <soledispafg@hotmail.com>
2024-11-29 05:34:40 -0500

    primer commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,21 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+   nombre: string = '';
+  edad: number | null = null;
+  mensaje: { titulo: string; texto: string } | null = null;
+
+  verificarEdad() {
+    if (this.edad === null || this.edad < 0) {
+      this.mensaje = { titulo: 'Error', texto: 'Por favor ingresa una edad válida.' };
+      return;
+    }
+
+    if (this.edad < 18) {
+      this.mensaje = { titulo: 'Resultado', texto: `${this.nombre}, eres menor de edad.` };
+    } else {
+      this.mensaje = { titulo: 'Resultado', texto: `${this.nombre}, eres mayor de edad.` };
+    }
+  }
 
 }
